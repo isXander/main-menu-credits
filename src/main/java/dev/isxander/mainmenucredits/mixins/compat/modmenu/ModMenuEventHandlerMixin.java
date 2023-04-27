e73b32d318aa9c0ca991f56d760e870a9e9d4ce8
isXander <xandersmith2008@gmail.com>
2023-04-27 14:04:05 +0100

    fix

--- src/main/java/dev/isxander/mainmenucredits/mixins/compat/modmenu/ModMenuEventHandlerMixin.java
+++ src/main/java/dev/isxander/mainmenucredits/mixins/compat/modmenu/ModMenuEventHandlerMixin.java
@@ -9,19 +9,24 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Surrogate;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Pseudo
 @Mixin(value = ModMenuEventHandler.class)
 public class ModMenuEventHandlerMixin {
+    // for ModMenu <6.2.0
+    @Surrogate
     @Inject(method = "shiftButtons(Lnet/minecraft/client/gui/widget/ClickableWidget;ZI)V", at = @At("HEAD"), cancellable = true, require = 0)
-    private static void cancelShiftButtonsMM610(ClickableWidget button, boolean shiftUp, int spacing, CallbackInfo ci) {
+    private static void cancelShiftButtons(ClickableWidget button, boolean shiftUp, int spacing, CallbackInfo ci) {
         if (button instanceof TextWidget)
             ci.cancel();
     }
 
+    // for ModMenu >=6.2.0
+    @Surrogate
     @Inject(method = "shiftButtons(Lnet/minecraft/client/gui/widget/Widget;ZI)V", at = @At("HEAD"), cancellable = true, require = 0)
-    private static void cancelShiftButtonsMM620(Widget widget, boolean shiftUp, int spacing, CallbackInfo ci) {
+    private static void cancelShiftButtons(Widget widget, boolean shiftUp, int spacing, CallbackInfo ci) {
         if (widget instanceof TextWidget)
             ci.cancel();
     }
